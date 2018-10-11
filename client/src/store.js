ced918bd10dfe42564c90db42440911a439aa386
Taylor Jewell <taylorjewell@outlook.com>
2018-10-11 01:56:01 -0400

    Version 6

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,7 @@
-import {
-  createStore,
-  applyMiddleware,
-  composeWithDevTools
-} from "redux-devtools-extension/developmentOnly";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import composeWithDevTools from "redux-devtools-extension/developmentOnly";
 
 const initialState = {};
 
