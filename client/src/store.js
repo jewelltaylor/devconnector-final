6931f82a96414d672ab2eb3821290f72f17f5f1d
Taylor Jewell <taylorjewell@outlook.com>
2018-10-11 01:50:37 -0400

    Version 5

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,10 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import {
+  createStore,
+  applyMiddleware,
+  composeWithDevTools
+} from "redux-devtools-extension/developmentOnly";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
@@ -9,7 +13,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
+  composeWithDevTools(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
